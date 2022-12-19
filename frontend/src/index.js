bff4c79f38052a52349227f9584fdd52a780800e
kkj2010 <kunjukim0210@gmail.com>
2022-12-18 20:35:25 -0500

    fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import reportWebVitals from './reportWebVitals';
-import configureStore from './store/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import configureStore from "./store/store";
 
-const store= configureStore({};)
+const store = configureStore({});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
