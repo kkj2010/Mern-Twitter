b05d3c4a285e1d24c0356719a574be0cae442098
kkj2010 <kunjukim0210@gmail.com>
2022-12-18 20:14:08 -0500

    create signup and reducer action

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
-import { Switch } from "react-router-dom";
-import { AuthRoute, ProtectedRoute } from "./components/Routes/Routes";
-import "./App.css";
-import MainPage from "./components/MainPage/MainPage";
-import LoginForm from "./components/MainPage/LoginForm";
-import SignupForm from "./components/MainPage/SignupForm";
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Switch } from 'react-router-dom';
+
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
 
+import MainPage from './components/MainPage/MainPage';
+import LoginForm from './components/SessionForms/LoginForm';
+import SignupForm from './components/SessionForms/SignupForm';
+
+import { getCurrentUser } from './store/session';
+
 function App() {
-  return (
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser()).then(() => setLoaded(true));
+  }, [dispatch]);
+
+  return loaded && (
     <>
-      <NavBar/>
+      <NavBar />
       <Switch>
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
@@ -18,4 +29,3 @@ function App() {
     </>
   );
 }
-export default App;
